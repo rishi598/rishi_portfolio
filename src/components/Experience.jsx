3b00a66c92d229bee1958ee3a53fa8436d02400b
Rishi <78724092+rishi598@users.noreply.github.com>
2025-04-20 15:53:16 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,7 @@ import { styles } from '../styles'
 import { experiences } from '../constants'
 import { SectionWrapper } from '../hoc'
 import { textVariant } from "../utils/motion"
-import { div } from "framer-motion/client"
+
 
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement 
